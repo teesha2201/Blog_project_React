cfd0bc49acf5059f5d2cc9c8853a918e08ff9a31
teesha2201 <teesha2201@gmail.com>
2023-09-22 15:56:03 +0530

    again

--- src/Store/RouteCompo.js
+++ src/Store/RouteCompo.js
@@ -1,5 +1,5 @@
 
-import React  , {usestate} from 'react'
+import React   from 'react'
 import {Routes ,Route, BrowserRouter ,NavLink} from 'react-router-dom';
 import Home from '../Component/Home'
 import Bollywood from '../Component/Bollywood'
@@ -70,44 +70,7 @@ function   RouteCompo()
         </> 
         )
           
-            {/* <button className="navbar-toggle" onClick={toggleNavbar}>
-                <span className="navbar-toggle-icon">&#9776;</span>
-            </button>
-            <div className={`MenuIcon ${isOpen ?'open' :''}`}>
-                
-            </div> */}
-       
-        
-       
                  
-             {/* <BrowserRouter>
-                 <StoreContextApi>                     
-                    <Routes>
-                         <Route exact path='/' element={<Home/>}/>
-                         <Route exact path='/Bollywood' element={<Bollywood/>}/>                         
-                         <Route exact path='/Technology' element={<Technology/>}/>
-                         <Route exact path='/Hollywood' element={<Hollywood/>}/>
-                         <Route exact path='/Fitness' element={<Fitness/>}/>
-                         <Route exact path='/Food' element={<Food/>}/>
-                         <Route exact path='/details/:id' element={<Details/>}></Route>
-                     </Routes>
-                 </StoreContextApi>    
-             */}
-
-                 {/* <div className='displaynav'><i class="fa-solid fa-bars"></i>
-                 <header className='HeaderNavlink'>  
-                    <section>
-                     <span className='The_title' >The</span>
-                     <h1 className='siren'>Siren</h1>
-                 </section>
-                     <NavLink className="navlink" to='/'>Home </NavLink>
-                     <NavLink className="navlink" to='/Bollywood'>Bollywood</NavLink>
-                     <NavLink className="navlink" to='/Technology'>Technology</NavLink>                      
-                     <NavLink className="navlink" to='/Hollywood'>Hollywood</NavLink>
-                     <NavLink className="navlink" to='/Fitness'>Fitness</NavLink>
-                     <NavLink className="navlink" to='/Food'>Food</NavLink>
-                 </header>
-                 </div> */}        
     
 }
 export default  RouteCompo
